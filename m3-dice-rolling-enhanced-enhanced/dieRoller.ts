d4d3a1c5c3d58456dc4b39e9521fade43392bbd1
Maciej Perdenia <maciej.perdenia@gmail.com>
2019-02-03 23:31:11 +0100

    [M4] Initial commit

---
+++ m3-dice-rolling-enhanced-enhanced/dieRoller.ts
@@ -0,0 +1,71 @@
+"use strict";
+
+import * as _ from 'lodash';
+import * as Chance from 'chance';
+
+var chance = new Chance();
+
+enum DieValue {
+  None,
+  One,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six
+}
+
+interface DieInterface {
+  div: Element;
+  width: number;
+  height: number;
+  border: number; // implicitly solid black
+  color: string;
+}
+
+class Die implements DieInterface {
+  static DieValue = DieValue;
+
+  constructor(
+    public div: Element,
+    public width: number,
+    public height: number,
+    public border: number, // implicitly solid black
+    public color: string,
+    protected value: DieValue = DieValue.None
+  ) {
+    (this.div as HTMLElement).style.width = `${this.width}px`;
+    (this.div as HTMLElement).style.height = `${this.height}px`;
+    (this.div as HTMLElement).style.border = `${this.border}px solid black`;
+    (this.div as HTMLElement).style.backgroundColor = color;
+    (this.div as HTMLElement).style.display = "table-cell";
+    (this.div as HTMLElement).style.textAlign = "center";
+    (this.div as HTMLElement).style.verticalAlign = "middle";
+    (this.div as HTMLElement).style.fontSize = "1.5em";
+    (this.div as HTMLElement).style.fontWeight = "bold";
+  }
+
+  append: Function = (target: Element): boolean => {
+    (target as HTMLElement).appendChild(this.div);
+    return true;
+  };
+
+  setValue: Function = (value: number): boolean => {
+    this.value = (value as DieValue);
+    (this.div as HTMLElement).innerText = DieValue[value];
+    return true;
+  };
+}
+
+class DieRoller extends Die {
+  constructor(div: Element, length: number, width: number, border: number, color: string) {
+    super(div, length, width, border, color);
+  }
+
+  roll: Function = (): boolean => {
+    this.setValue(chance.d6());
+    return true;
+  };
+}
+
+export default DieRoller;
---
+++ m3-dice-rolling-enhanced-enhanced/main.ts
@@ -0,0 +1,41 @@
+import DieRoller from './dieRoller.js';
+import * as _ from 'lodash';
+
+function createDice(size: number, border: number, color: string): Array<DieRoller> {
+  let dice: Array<DieRoller> = [];
+  _.forEach(_.range(0, 4), () => {
+    let div: Element = document.createElement('div');
+    let die: DieRoller = new DieRoller(div, size, size, border, color);
+    die.append(diceTable);
+    dice.push(die);
+  });
+  return dice;
+}
+
+namespace DieStyling {
+  export let size: number = 100;
+  export let border: number = 5;
+  export let color: string = 'orange';
+}
+
+let diceTable: Element = document.createElement('div');
+(diceTable as HTMLElement).style.display = 'table';
+(diceTable as HTMLElement).style.borderSpacing = '15px';
+document.body.appendChild(diceTable);
+
+let dice: Array<DieRoller> = createDice(DieStyling.size, DieStyling.border, DieStyling.color);
+
+let buttonCell: Element = document.createElement('div');
+(buttonCell as HTMLElement).style.display = 'table-cell';
+(buttonCell as HTMLElement).style.verticalAlign = 'middle';
+diceTable.appendChild(buttonCell);
+
+let rollButton: Element = document.createElement('button');
+rollButton.textContent = 'Roll the dice';
+(rollButton as HTMLElement).onclick = () => {
+  _.forEach(dice, (die) => {
+    die.roll();
+  });
+};
+(rollButton as HTMLElement).style.fontSize = '1.25em';
+buttonCell.appendChild(rollButton);
